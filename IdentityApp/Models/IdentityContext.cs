3dd2fbaf639570c10a50aee616c57601e5ae1a26
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-04 18:12:32 +0300

    Add project files.

---
+++ IdentityApp/Models/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public class IdentityContext : IdentityDbContext<IdentityUser>
+    {
+        // Constructor that accepts DbContextOptions and passes it to the base class
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+
+        }
+        
+    }
+}
