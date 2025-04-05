bd784bf5bdac10e7346b49941b59bc5e04012aa0
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-05 12:06:58 +0300

    feat: Add Create New Role function to Roles page

---
+++ IdentityApp/Controllers/RolesController.cs
@@ -0,0 +1,43 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AppRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(role);
+        }
+    }
+}
