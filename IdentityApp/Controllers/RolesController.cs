f4675ef6a46c7b0ea47765f4848cab922fc82f3e
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-05 14:20:02 +0300

    feat: Add Role List to Role Index page

--- IdentityApp/Controllers/RolesController.cs
+++ IdentityApp/Controllers/RolesController.cs
@@ -35,6 +35,7 @@ namespace IdentityApp.Controllers
                 {
                     ModelState.AddModelError("", error.Description);
                 }
+                i
             }
 
             return View(role);
