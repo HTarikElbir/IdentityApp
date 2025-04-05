21646329f82bc7432c4d5f2dd199e406f2e073a3
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-05 11:19:50 +0300

    feat: Add User Delete function to Index page

--- IdentityApp/Controllers/UsersController.cs
+++ IdentityApp/Controllers/UsersController.cs
@@ -7,12 +7,12 @@ namespace IdentityApp.Controllers
 {
     public class UsersController : Controller
     {
-        
+
         private UserManager<AppUser> _userManager;
-        
+
         public UsersController(UserManager<AppUser> userManager)
         {
-            _userManager = userManager; 
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -20,7 +20,7 @@ namespace IdentityApp.Controllers
             return View(_userManager.Users);
         }
 
-        public  IActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -28,7 +28,7 @@ namespace IdentityApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateViewModel model)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 var user = new AppUser
                 {
@@ -50,6 +50,81 @@ namespace IdentityApp.Controllers
                 }
             }
             return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                return View(new EditViewModel
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email,
+                });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, EditViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.Id);
+
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.FullName = model.FullName;
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if (result.Succeeded && !string.IsNullOrEmpty(model.Password))
+                    {
+                        await _userManager.RemovePasswordAsync(user);
+                        await _userManager.AddPasswordAsync(user, model.Password);
+                    }
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                var user = await _userManager.FindByIdAsync(id);
+                if (user != null)
+                {
+                    var result = await _userManager.DeleteAsync(user);
+                   
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
